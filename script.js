831a4b4f7349ce9f89f421474a88b5856fde4260
Unknownmaster0 <sagar192002singh@gmail.com>
2024-08-16 18:54:33 +0530

    blury-image-js-practice-day-04

---
+++ script.js
@@ -0,0 +1,30 @@
+const img = document.querySelector('.bg');
+const text = document.querySelector('.text');
+
+let count = 10;
+let i = 0;
+
+img.style.filter = 'blur(10px)';
+
+document.addEventListener('DOMContentLoaded', () => {
+  updateBlur();
+});
+
+function updateBlur() {
+  if (i <= 100) {
+    text.textContent = `${i}%`;
+    text.style.filter = `blur(${i}%)`;
+  }
+
+  if (i % 10 === 0) {
+    img.style.filter = `blur(${100 - i}px)`;
+  }
+  i++;
+  if (i >= 100) text.classList.add('hidden');
+  setTimeout(updateBlur, 20);
+}
+
+// work after some time or taking more time than document object
+// window.onload = () => {
+//   console.log(`page is loaded successfully😁`);
+// };
